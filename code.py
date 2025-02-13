de1e5cd324e638300fb3106f036267e035523b7e
Allergicboe <91279649+Allergicboe@users.noreply.github.com>
2025-02-13 11:33:29 -0300

    Update code.py

--- code.py
+++ code.py
@@ -73,10 +73,13 @@ def main():
         st.write(f"**Campo:** {row_data[3]} [ID: {row_data[2]}]")
     with col2:
         st.write(f"**Sonda:** {row_data[10]} [ID: {row_data[11]}]")
-    
-    # Fila para los enlaces
-    st.write(f"[Ver campo](https://www.dropcontrol.com/site/dashboard/campo.do?cuentaId={row_data[0]}&campoId={row_data[2]})")  # Enlace de campo
-    st.write(f"[Ver sonda](https://www.dropcontrol.com/site/ha/suelo.do?cuentaId={row_data[0]}&campoId={row_data[2]}&sectorId={row_data[11]})")  # Enlace de sonda
+
+    # Fila para los enlaces (en la misma línea)
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.write(f"[Ver campo](https://www.dropcontrol.com/site/dashboard/campo.do?cuentaId={row_data[0]}&campoId={row_data[2]})")  # Enlace de campo
+    with col2:
+        st.write(f"[Ver sonda](https://www.dropcontrol.com/site/ha/suelo.do?cuentaId={row_data[0]}&campoId={row_data[2]}&sectorId={row_data[11]})")  # Enlace de sonda
 
 
 # --- 6. Formulario de Edición ---
