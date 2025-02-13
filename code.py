d016838b46ff584e9445d25d4b8e9401d0020c26
Allergicboe <91279649+Allergicboe@users.noreply.github.com>
2025-02-13 14:31:57 -0300

    Update code.py

--- code.py
+++ code.py
@@ -76,14 +76,18 @@ def main():
     # --- 6. Mostrar Información Básica de la Fila ---
     st.subheader("Información de la fila seleccionada")
     col1, col2 = st.columns([1, 1])
+    
     with col1:
         st.write(f"**Cuenta:** {row_data[1]} [ID: {row_data[0]}]")
-        st.write(f"**Campo:** {row_data[3]} [ID: {row_data[2]}]")
-        st.markdown(
-            f"[Ver Campo](https://www.dropcontrol.com/site/dashboard/campo.do?cuentaId={row_data[0]}&campoId={row_data[2]})"
-            ", "
-            f"[Ver Sonda](https://www.dropcontrol.com/site/ha/suelo.do?cuentaId={row_data[0]}&campoId={row_data[2]}&sectorId={row_data[11]})"
+        st.write(f"**Campo:** {row_data[3]} [ID: {row_data[2]}")
+        st.write(
+            "[Ver Campo](https://www.dropcontrol.com/site/dashboard/campo.do"
+            f"?cuentaId={row_data[0]}&campoId={row_data[2]})"
+            " , "
+            "[Ver Sonda](https://www.dropcontrol.com/site/ha/suelo.do"
+            f"?cuentaId={row_data[0]}&campoId={row_data[2]}&sectorId={row_data[11]})"
         )
+    
     with col2:
         st.write(f"**Sonda:** {row_data[10]} [ID: {row_data[11]}]")
         st.write(f"**Comentario:** {row_data[39]}")
